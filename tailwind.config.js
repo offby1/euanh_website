b277424f09cd95c245de7997db5480f4319abb18
EuanHoll <ehollidge@hotmail.com>
2023-12-18 23:04:51 +0100

    Logo and Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["euanh_website/templates/**/*.html"],
+  content: ["euanh_website/templates/**/*.jinja"],
   theme: {
     extend: {
+      colors:{
+        "header_green": "#9fb1b1",
+        "header_green_hover": "#73a499",
+      },
+      fontFamily: {
+        riffic: ["riffic", 'sans-serif'],
+      }
     },
   },
   plugins: [],
 }
-
