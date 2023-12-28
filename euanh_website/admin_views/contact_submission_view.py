2801bb80ab41fd2a7c376b09c6d4488e1188fd3e
EuanHoll <ehollidge@hotmail.com>
2023-12-28 20:17:41 +0100

    Adding Read and Spam

--- euanh_website/admin_views/contact_submission_view.py
+++ euanh_website/admin_views/contact_submission_view.py
@@ -11,7 +11,115 @@ class ContactSubmissionView(ModelView, model=ContactSubmission):
         ContactSubmission.id,
         ContactSubmission.name,
         ContactSubmission.email,
+        ContactSubmission.read,
+        ContactSubmission.spam,
         ContactSubmission.created_on,
     ]
 
     form_excluded_columns = ["created_on", "updated_on"]
+
+    column_default_sort = ("created_on", True)
+
+    @action(
+        name="mark_as_read",
+        label="Mark as Read",
+        confirmation_message="Are you sure you want to mark these submissions as read?",
+        add_in_detail=True,
+        add_in_list=True,
+    )
+    def action_mark_as_read(self, request: Request) -> RedirectResponse:
+        pks = request.query_params.get("pks", "").split(",")
+        if pks:
+            pks = [int(pk) for pk in pks]
+            with Session() as session:
+                models = (
+                    session.query(ContactSubmission)
+                    .filter(ContactSubmission.id.in_(pks))
+                    .all()
+                )
+                for model in models:
+                    model.read = True
+                session.commit()
+
+        referer = request.headers.get("Referer")
+        if referer:
+            return RedirectResponse(referer)
+        return RedirectResponse(request.url_for("admin:list", identity=self.identity))
+
+    @action(
+        name="mark_as_unread",
+        label="Mark as Unread",
+        confirmation_message="Are you sure you want to mark these submissions as unread?",
+        add_in_detail=True,
+        add_in_list=True,
+    )
+    def action_mark_as_unread(self, request: Request) -> RedirectResponse:
+        pks = request.query_params.get("pks", "").split(",")
+        if pks:
+            pks = [int(pk) for pk in pks]
+            with Session() as session:
+                models = (
+                    session.query(ContactSubmission)
+                    .filter(ContactSubmission.id.in_(pks))
+                    .all()
+                )
+                for model in models:
+                    model.read = False
+                session.commit()
+
+        referer = request.headers.get("Referer")
+        if referer:
+            return RedirectResponse(referer)
+        return RedirectResponse(request.url_for("admin:list", identity=self.identity))
+
+    @action(
+        name="mark_as_spam",
+        label="Mark as Spam",
+        confirmation_message="Are you sure you want to mark these submissions as spam?",
+        add_in_detail=True,
+        add_in_list=True,
+    )
+    def action_mark_as_spam(self, request: Request) -> RedirectResponse:
+        pks = request.query_params.get("pks", "").split(",")
+        if pks:
+            pks = [int(pk) for pk in pks]
+            with Session() as session:
+                models = (
+                    session.query(ContactSubmission)
+                    .filter(ContactSubmission.id.in_(pks))
+                    .all()
+                )
+                for model in models:
+                    model.spam = True
+                session.commit()
+
+        referer = request.headers.get("Referer")
+        if referer:
+            return RedirectResponse(referer)
+        return RedirectResponse(request.url_for("admin:list", identity=self.identity))
+
+    @action(
+        name="mark_as_not_spam",
+        label="Mark as Not Spam",
+        confirmation_message="Are you sure you want to mark these submissions as not spam?",
+        add_in_detail=True,
+        add_in_list=True,
+    )
+    def action_mark_as_not_spam(self, request: Request) -> RedirectResponse:
+        pks = request.query_params.get("pks", "").split(",")
+        if pks:
+            pks = [int(pk) for pk in pks]
+            with Session() as session:
+                models = (
+                    session.query(ContactSubmission)
+                    .filter(ContactSubmission.id.in_(pks))
+                    .all()
+                )
+                for model in models:
+                    model.spam = False
+                session.commit()
+
+        referer = request.headers.get("Referer")
+        if referer:
+            return RedirectResponse(referer)
+        return RedirectResponse(request.url_for("admin:list", identity=self.identity))
--- euanh_website/models/contact_submission.py
+++ euanh_website/models/contact_submission.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Boolean, Column, Integer, String, Text
 
 from euanh_website.models.base import CommonBase
 
@@ -10,6 +10,8 @@ class ContactSubmission(CommonBase):
     name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
     message = Column(Text, nullable=False)
+    read = Column(Boolean, nullable=False, default=False)
+    spam = Column(Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"<ContactSubmission(name='{self.name}', email='{self.email}')>"
